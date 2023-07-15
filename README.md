8798b714f73f54caf6dbd03f678d5b8c678c2977
Kostya Valsamaki <jormungand.beat@gmail.com>
2023-07-15 15:55:47 +0300

    readme.md + test_all.py modifications
    in test_all.py created 3 autotests where 2 passed and 1 skipped, tests are created using pytest, selenium and allure-pytest libraries, in third test 'goto_webdriver_page' you can see attachment adding to the report. also allure.severity fixtures are used for these three tests. \n\nIn readme.md added all prequisites so you can setup envirnment same as it was on my machine

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
+Prquisites:
+download python
+If you're on linux run this command in terminal:
+"$ sudo apt-get update python
+$ sudo apt-get install python3.6"
+
+If you're on windows machine
+go to >> https://www.python.org/downloads/
+
+to check if python is installed run:
+"python --version"
+
 prequisites: 
 1. pip install selenium
 2. pip install pytest
@@ -8,5 +20,14 @@ when scoop is installed:
 run command in PowerShell >> scoop install allure
 
 this will automatically install allure on your windows machine
+(also you'll need java runtime for that download here: https://www.java.com/download/ie_manual.jsp)
 
 than start creating your autotests
+
+when autotests are created run a command: 
+"pytest -v -s --alluredir="C:\prog\for_allure\allure_reports" test_all.py"
+
+when autotests are done run: 
+"allure serve {path to reports}"
+
+tests will be running and allure reports will be generated
--- test_all.py
+++ test_all.py
@@ -1,3 +1,42 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from allure_commons.types import AttachmentType
 import allure
 import pytest
+
+@allure.severity(allure.severity_level.NORMAL)
+class TestSelenium:
+
+    @allure.severity(allure.severity_level.MINOR)
+    def test_check_working(self):
+        self.driver = webdriver.Chrome()
+        self.driver.implicitly_wait(1)
+        self.driver.get('https://www.selenium.dev/')
+        status = self.driver.find_element(By.XPATH, '//*[local-name()="svg" and @id="selenium_webdriver"]').is_displayed()
+        
+        if status: assert True
+        else: assert False
+
+        self.driver.close()
+    
+    @allure.severity(allure.severity_level.NORMAL)
+    def test_another(self):
+        pytest.skip('This test will be implemented later...')
+
+    @allure.severity(allure.severity_level.CRITICAL)    
+    def test_goto_webdriver_page(self):
+        self.driver = webdriver.Chrome()
+        self.driver.implicitly_wait(1)
+        self.driver.get('https://www.selenium.dev/')
+        link =self.driver.find_element(By.XPATH,'//a[contains(@class, "webdriver")]').get_attribute('href')
+        self.driver.get(link)
+        status = self.driver.find_element(By.XPATH,'//h1[text()="WebDriver"]').is_displayed()
+        
+        if status: 
+            allure.attach(self.driver.get_screenshot_as_png(), name='webdriverPageScreen', attachment_type=AttachmentType.PNG) 
+            assert True
+        else:
+            allure.attach(self.driver.get_screenshot_as_png(), name='webdriverPageScreen', attachment_type=AttachmentType.PNG) 
+            assert False
+        
+        self.driver.close()
